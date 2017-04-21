fe96a037b6de32d04e86411dbc5bd34eb3d729c2
nheise <niklas.heise@ratepay.com>
2017-04-21 15:37:56 +0200

    implement and test withAttr function

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
+var m = require( "mithril" )
 
 function AttributeBuilder( givenAttrs ) {
-  const attrs = givenAttrs || {};
+  const attrs = givenAttrs || {}
   
   function classes( classAttrs ) {
-    const whitespace = ' ';
-    attrs["class"] =  Object.keys( classAttrs ).filter( key => !!classAttrs[key] ).join(whitespace);
-    return this;
+    const whitespace = ' '
+    attrs["class"] =  Object.keys( classAttrs ).filter( key => !!classAttrs[key] ).join(whitespace)
+    return this
   }
   
   function onclick( fn ) {
-    set( "onclick", fn );
-    return this;
+    set( "onclick", fn )
+    return this
   }
   
   function set( key, value ) {
-    attrs[key] = value;
-    return this;
+    attrs[key] = value
+    return this
+  }
+  
+  function withAttr( attrName, event, initialValue, callback, thisArg ) {
+    attrs[attrName] = initialValue
+    attrs[event] = m.withAttr( attrName, callback, thisArg )
+    return this
   }
   
   function getAttrs() {
-    return attrs;
+    return attrs
   }
   
   return {
     classes: classes,
     getAttrs : getAttrs,
     set: set,
-    onclick: onclick
-  };
+    onclick: onclick,
+    withAttr: withAttr
+  }
 }
 
-module.exports = givenAttrs => new AttributeBuilder( givenAttrs );
+module.exports = givenAttrs => new AttributeBuilder( givenAttrs )
--- tests/util-test.js
+++ tests/util-test.js
@@ -1,45 +1,84 @@
-var o = require("ospec");
+var o = require("ospec")
+var browserMock = require("mithril/test-utils/browserMock")
 
-var ma = require('../index.js');
+window = browserMock()
 
 o.spec("mithril-util-attributes", function() {
+
+  var ma
+
+  o.beforeEach(function() {
+    var mock = browserMock()
+    if (typeof global !== "undefined") {
+      global.window = mock
+    }
+    ma = require('../index.js')
+  })
   
   o("ma without any givings, must create empty object", function() {
-    var attr = ma().getAttrs();
-    o( attr ).deepEquals( {} );
-  });
+    var attr = ma().getAttrs()
+    o( attr ).deepEquals( {} )
+  })
 
   o("ma with an object given, but no further invocations, must return given object", function() {
-    var given = { myAttr: "myValue" };
-    var attr = ma( given ).getAttrs();
-    o( attr ).deepEquals( given );
-  });
+    var given = { myAttr: "myValue" }
+    var attr = ma( given ).getAttrs()
+    o( attr ).deepEquals( given )
+  })
 
   o("ma set attribute, must set attribute", function() {
-    var fn = () => true;
-    var attrs = ma().set( "attr", fn ).getAttrs();
-    o( attrs.attr ).deepEquals( fn );
-  });
+    var fn = () => true
+    var attrs = ma().set( "attr", fn ).getAttrs()
+    o( attrs.attr ).deepEquals( fn )
+  })
   
   o("ma onclick, must set onclick function", function() {
-    var fn = () => true;
-    var attrs = ma().onclick( fn ).getAttrs();
-    o( attrs.onclick ).deepEquals( fn );
-  });
+    var fn = () => true
+    var attrs = ma().onclick( fn ).getAttrs()
+    o( attrs.onclick ).deepEquals( fn )
+  })
 
   o.spec("ma classes", function() {
     o("when attribute is evaluate to true, use it as css class", function() {
-      var attr = ma().classes( { 'my-class': true } ).getAttrs();
-      o( attr["class"] ).equals( "my-class" );
-    });
+      var attr = ma().classes( { 'my-class': true } ).getAttrs()
+      o( attr["class"] ).equals( "my-class" )
+    })
     o("when attribute is evaluate to false, don't use it as css class", function() {
-      var attr = ma().classes( { 'my-class': false } ).getAttrs();
-      o( attr["class"] ).equals( "" );
-    });
+      var attr = ma().classes( { 'my-class': false } ).getAttrs()
+      o( attr["class"] ).equals( "" )
+    })
     o("when two or more attribute are evaluate to true, connect them as css class", function() {
-      var attr = ma().classes( { 'my-class': true, 'my-class2': false, 'my-class3': true } ).getAttrs();
-      o( attr["class"] ).equals( "my-class my-class3" );
-    });
-  });
+      var attr = ma().classes( { 'my-class': true, 'my-class2': false, 'my-class3': true } ).getAttrs()
+      o( attr["class"] ).equals( "my-class my-class3" )
+    })
+  })
   
-});
+  o.spec("withAttr must set", function() {
+    o("attr value and event, without thisArg", function() {
+      var testValue
+      var fn = (value) => testValue = value
+      
+      var attrs = ma().withAttr( "value", "oninput", "myValue", fn ).getAttrs()
+      
+      o( attrs.value ).equals( "myValue" )
+      
+      var event = { currentTarget: { value: "newValue" } }
+      attrs.oninput( event )
+      o( testValue ).equals( "newValue" )
+    })
+    o("attr value and event, with thisArg", function() {
+      var testValue, context
+      function fn(value){ testValue = value; context = this; }
+      
+      var attrs = ma().withAttr( "value", "oninput", "myValue", fn, { name: "myname" } ).getAttrs()
+      
+      o( attrs.value ).equals( "myValue" )
+      
+      var event = { currentTarget: { value: "newValue" } }
+      attrs.oninput( event )
+      o( testValue ).equals( "newValue" )
+      o( context.name ).equals( "myname" )
+    })
+  })
+  
+})
